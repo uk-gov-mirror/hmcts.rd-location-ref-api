4b5a9315cfd74098bf9a3b9d0ff668e54381e100
abhijit <Abhijit.diwan@hmcts.net>
2020-08-21 14:02:38 +0100

    feat: Initial commit rd location ref data

--- src/main/java/uk/gov/hmcts/reform/locationrefapi/controllers/WelcomeController.java
+++ src/main/java/uk/gov/hmcts/reform/locationrefapi/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.demo.controllers;
+package uk.gov.hmcts.reform.locationrefapi.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +10,7 @@ import static org.springframework.http.ResponseEntity.ok;
  * Default endpoints per application.
  */
 @RestController
-public class RootController {
+public class WelcomeController {
 
     /**
      * Root GET endpoint.
@@ -23,6 +23,6 @@ public class RootController {
      */
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
-        return ok("Welcome to spring-boot-template");
+        return ok("Welcome to location ref api");
     }
 }
