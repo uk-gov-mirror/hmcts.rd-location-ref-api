7c45c2d43c1b89ae3ba2a611e51ad84d0315f9f1
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-08-26 16:21:27 +0100

    Add Infra and chart config

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,17 +1,31 @@
-variable "product" {}
+variable "product" {
+  type = "string"
+}
 
-variable "component" {}
+variable "component" {
+  type = "string"
+}
 
 variable "location" {
+  type    = "string"
   default = "UK South"
 }
 
-variable "env" {}
+variable "env" {
+  type = "string"
+}
 
-variable "subscription" {}
+variable "subscription" {
+  type = "string"
+}
 
-variable "deployment_namespace" {}
+// variable "deployment_namespace" {}
 
 variable "common_tags" {
   type = "map"
+}
+
+variable "postgresql_version" {
+  type    = "string"
+  default = "11"
 }
