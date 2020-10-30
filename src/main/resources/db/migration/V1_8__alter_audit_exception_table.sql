4b56467099b308e44df0a56f0370e25ca460c388
abhijit <Abhijit.diwan@hmcts.net>
2020-10-30 14:22:53 +0000

    refactor: Updated flyway for data-ingestion library changes

--- src/main/resources/db/migration/V1_8__alter_audit_exception_table.sql
+++ src/main/resources/db/migration/V1_8__alter_audit_exception_table.sql
@@ -1,7 +1,3 @@
-truncate  dataload_schedular_audit;
-
-truncate dataload_exception_records;
-
 ALTER TABLE dataload_schedular_audit  RENAME COLUMN scheduler_status TO status;
 
 ALTER TABLE dataload_schedular_audit ADD COLUMN file_name varchar(128);
