d17f8feb02aaa1240765758e325c9f6a5609d8a0
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-10-31 16:14:02 +0000

    feat:schema issue

--- src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/LrdAuthorizationEnabledIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/LrdAuthorizationEnabledIntegrationTest.java
@@ -49,7 +49,6 @@ import static uk.gov.hmcts.reform.lrdapi.util.JwtTokenUtil.getUserIdAndRoleFromT
 @RunWith(SpringIntegrationSerenityRunner.class)
 @WithTags({@WithTag("testType:Integration")})
 @TestPropertySource(properties = {"S2S_URL=http://127.0.0.1:8990", "IDAM_URL:http://127.0.0.1:5000"})
-@TestPropertySource("classpath:application.yml")
 @DirtiesContext
 public abstract class LrdAuthorizationEnabledIntegrationTest extends SpringBootIntegrationTest {
 
--- src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/SpringBootIntegrationTest.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.reform.lrdapi;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LrdApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"spring.config.location=classpath:application-integration.yml"})
 public abstract class SpringBootIntegrationTest {
 
     @LocalServerPort
