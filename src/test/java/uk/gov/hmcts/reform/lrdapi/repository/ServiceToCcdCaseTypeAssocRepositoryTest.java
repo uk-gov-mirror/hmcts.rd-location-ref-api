2385e0704a0c84f70e6812ef02901c7fdf8612d9
abhijit <Abhijit.diwan@hmcts.net>
2020-11-02 11:17:27 +0000

    fix: JPA mock repository test in junit

---
+++ src/test/java/uk/gov/hmcts/reform/lrdapi/repository/ServiceToCcdCaseTypeAssocRepositoryTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.lrdapi.repository;
+
+import org.junit.Test;
+import uk.gov.hmcts.reform.lrdapi.domain.ServiceToCcdCaseTypeAssoc;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ServiceToCcdCaseTypeAssocRepositoryTest {
+
+    ServiceToCcdCaseTypeAssocRepositry serviceToCcdCaseTypeAssocRepositry
+        = mock(ServiceToCcdCaseTypeAssocRepositry.class);
+
+    @Test
+    public void findByServiceCodeTest() {
+        when(serviceToCcdCaseTypeAssocRepositry.findByCcdCaseType(anyString()))
+            .thenReturn(new ServiceToCcdCaseTypeAssoc());
+        assertNotNull(serviceToCcdCaseTypeAssocRepositry.findByCcdCaseType(anyString()));
+    }
+}
