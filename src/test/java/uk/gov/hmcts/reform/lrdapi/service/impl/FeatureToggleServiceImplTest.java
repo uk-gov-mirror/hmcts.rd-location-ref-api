93af4b4346b9a671f5c7523828fd1fc3a4c13cd5
abhijit <Abhijit.diwan@hmcts.net>
2020-12-01 16:04:19 +0000

    refactor: toggle off  LRD API (RDCC-2011)

--- src/test/java/uk/gov/hmcts/reform/lrdapi/service/impl/FeatureToggleServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/lrdapi/service/impl/FeatureToggleServiceImplTest.java
@@ -15,7 +15,7 @@ public class FeatureToggleServiceImplTest {
     @Test
     public void testIsFlagEnabled() {
         flaFeatureToggleService = new FeatureToggleServiceImpl(ldClient, "rd");
-        assertFalse(flaFeatureToggleService.isFlagEnabled("lrd", "lrd-get-org-services-flag"));
+        assertFalse(flaFeatureToggleService.isFlagEnabled("lrd", "lrd-get-org-services-flag-mock"));
     }
 
     @Test
