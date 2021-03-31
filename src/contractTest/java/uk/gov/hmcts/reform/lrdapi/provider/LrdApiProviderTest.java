6bd93c095799d451bc4842eefda48dd2c87acb08
Ruban <ruban72@hotmail.com>
2021-03-31 09:35:47 +0100

    PAY-3900: Remove comment in provider test

--- src/contractTest/java/uk/gov/hmcts/reform/lrdapi/provider/LrdApiProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/lrdapi/provider/LrdApiProviderTest.java
@@ -66,7 +66,6 @@ public class LrdApiProviderTest {
 
     @BeforeEach
     void before(PactVerificationContext context) {
-        //System.getProperties().setProperty("pact.verifier.publishResults", "true");
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
         testTarget.setControllers(
             lrdApiController);
