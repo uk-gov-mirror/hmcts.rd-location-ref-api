52ed74f6705fc4a558b6358f715b05bd0f692e4f
Nitish Singla <nitish.singla@hmcts.net>
2021-04-09 16:28:25 +0100

    RDCC-2420: Updated repository class.

--- src/main/java/uk/gov/hmcts/reform/lrdapi/repository/ServiceToCcdCaseTypeAssocRepositry.java
+++ src/main/java/uk/gov/hmcts/reform/lrdapi/repository/ServiceToCcdCaseTypeAssocRepositry.java
@@ -3,7 +3,11 @@ package uk.gov.hmcts.reform.lrdapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.hmcts.reform.lrdapi.domain.ServiceToCcdCaseTypeAssoc;
 
+import java.util.List;
+
 public interface ServiceToCcdCaseTypeAssocRepositry extends JpaRepository<ServiceToCcdCaseTypeAssoc, Long> {
 
     ServiceToCcdCaseTypeAssoc findByCcdCaseTypeIgnoreCase(String ccdCaseType);
+
+    List<ServiceToCcdCaseTypeAssoc> findByCcdServiceNameInEqualsIgnoreCase(List<String> ccdServiceNames);
 }
